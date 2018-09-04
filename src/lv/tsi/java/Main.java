4292ef248bcaaf03d14b320efb7ee74c29418841
cekitedi <edite.cekita@gmai.com>
2018-09-04 17:09:28 +0300

    home update_2

--- src/lv/tsi/java/Main.java
+++ src/lv/tsi/java/Main.java
@@ -9,31 +9,33 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-      int myNum = rand.nextInt(100)+1;
-      boolean my_ok =false;
-      System.out.println(" Random = "+myNum);
-      for (int i = 1; i<6; i++)
-      {
-       // int myNum = rand.nextInt(100)+1;
-       System.out.println("Try nr # " +i);
-       int usernum =scan.nextInt();
+       String answer;
+     do {
+         int myNum = rand.nextInt(100) + 1;
+         boolean my_ok = false;
+         System.out.println(" Random = " + myNum);
 
-        if (myNum==usernum) {
-            System.out.println("BINGO!!");
-            my_ok =true;
-            break;
-        }
-        else
-           if (myNum<usernum ){
-                System.out.println("Значение " +myNum +" < " +usernum);
-           }
-           else {
-                System.out.println( "Значение " +myNum +" > " +usernum);
-           }
-      } // end for
-        if (!my_ok) {
-            System.out.println("You are loshara ");
-        }
+         for (int i = 1; i < 6; i++) {
+             // int myNum = rand.nextInt(100)+1;
+             System.out.println("Try nr # " + i);
+             int usernum = scan.nextInt();
+
+             if (myNum == usernum) {
+                 System.out.println("BINGO!!. Do You want repeat? yes - Y");
+                 my_ok = true;
+                 break;
+             } else if (myNum < usernum) {
+                 System.out.println("Значение " + myNum + " < " + usernum);
+             } else {
+                 System.out.println("Значение " + myNum + " > " + usernum);
+             }
+         } // end for
+         if (!my_ok) {
+             System.out.println("You are lost. Do You want repeat? yes - Y");
+         }
+
+          answer=scan.next();
+     } while (answer.equals("Y"));
     // end
     }
 }
